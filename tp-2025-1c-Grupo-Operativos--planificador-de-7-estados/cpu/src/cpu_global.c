3e29d49bba4e944409536ae8efcb108095298f2c
Cynthia Abbate <82001339+Cynthia-21@users.noreply.github.com>
2025-05-27 18:26:36 -0300

    Delete tp-2025-1c-Grupo-Operativos--planificador-de-7-estados directory

--- tp-2025-1c-Grupo-Operativos--planificador-de-7-estados/cpu/src/cpu_global.c
+++
@@ -1,9 +0,0 @@
-#include "cpu.h"
-
-t_log* cpu_logger = NULL;
-t_config* cpu_config = NULL;
-
-int socket_cpu = -1;
-int socket_memoria = -1;
-int socket_kernel_dispatch = -1;
-int socket_kernel_interrupt = -1;
